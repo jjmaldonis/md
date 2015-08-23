23bf76d51ffc1323d5cf0ee4434b9edeee720e6d
refreshx2 <jjmaldonis@gmail.com>
2015-08-23 14:07:50 -0500

    fixed cgm scripts and cgm'd the modelfile

--- scripts/cgm.py
+++ scripts/cgm.py
@@ -5,18 +5,14 @@ def main():
     modelfile = sys.argv[1]
     m = Model(modelfile)
     extension = modelfile.split('.')[-1]
-    if extension != 'dat':
-        modelfile = modelfile[modelfile.rfind('/')+1:]
-        modelfile = modelfile[:-4]+extension
-        m.write_dat('temp.dat')
-        modelfile = 'temp.dat'
+    m.write('temp.dat')
 
     of = open("cgm_temp.in", 'w')
     of.write("""units		metal
 boundary	p p p
 atom_style	atomic
 
-read_data {0}
+read_data temp.dat
 
 pair_style	eam/alloy
 pair_coeff	* * potentials/ZrCuAl2011.eam.alloy Zr Cu Al
@@ -25,7 +21,7 @@ thermo_style	custom step etotal fmax fnorm
 thermo		1
 
 minimize	1.0e-6 0 1000 10000
-write_data  lmp_emin_exp.dat""".format(modelfile))
+write_data  output.dat""")
     of.close()
 
 if __name__== '__main__':
--- scripts/cgm.sh
+++ scripts/cgm.sh
@@ -4,9 +4,12 @@ python scripts/cgm.py $@
 module load lammps-31Jan14
 lmp_linux < cgm_temp.in
 
-python /home/maldonis/model_analysis/scripts/model.py temp.dat -o xyz > post_cgm.xyz
+python /home/maldonis/model_analysis/scripts/model.py temp.dat -o xyz > output.xyz
 
 echo
-echo "WROTE FINAL XYZ FILE TO post_cgm.xyz"
-echo "Additional files written include: lmp_emin_exp.restart, log.lammps, cgm_temp.in, temp.dat"
+echo "WROTE FINAL XYZ FILE TO output.xyz"
+echo "Additional files written include: log.lammps, cgm_temp.in, temp.dat and they were automatically deleted!"
 echo
+rm log.lammps
+rm cgm_temp.in
+rm temp.dat
