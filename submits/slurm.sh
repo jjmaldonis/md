c5189d9ab9c3e21ccb68b0a8c509d40f2ad31602
Jason Maldonis <jjmaldonis@gmail.com>
2016-09-13 15:03:16 -0500

    updating submit file and adding NiP data

--- submits/slurm.sh
+++ submits/slurm.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-#SBATCH --job-name=ZrCuAl_MD                  # job name
-#SBATCH --partition=univ2                # default "univ" if not specified
+#SBATCH --job-name=NiP_MD                  # job name
+#SBATCH --partition=stem                # default "univ" if not specified
 #SBATCH --error=job.%J.err              # error file
 #SBATCH --output=job.%J.out             # output file
 
 #SBATCH --time=7-00:00:00               # run time in days-hh:mm:ss
 
-#SBATCH --nodes=4                      # number of nodes requested (n)
-#SBATCH --ntasks=4                     # required number of CPUs (n)
-#SBATCH --ntasks-per-node=1             # default 16 (Set to 1 for OMP)
-#SBATCH --cpus-per-task=16              # default 1 (Set to 16 for OMP)
-##SBATCH --mem=16384                    # total RAM in MB, max 64GB  per node
-##SBATCH --mem-per-cpu=4000              # RAM in MB (default 4GB, max 8GB)
+#SBATCH --nodes=1                      # number of nodes requested (n)
+#SBATCH --ntasks=16                     # required number of CPUs (n)
+#SBATCH --ntasks-per-node=16             # default 16 (Set to 1 for OMP)
+#SBATCH --cpus-per-task=1              # default 1 (Set to 16 for OMP)
 
 # set OMP_NUM_THREADS to the number of --cpus-per-task we asked for
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
@@ -29,8 +27,12 @@ echo "Number of cores per node: $SLURM_TASKS_PER_NODE"
 echo "Submit directory: $SLURM_SUBMIT_DIR"
 echo ""
 
+module load compile/intel
+module load mpi/intel/openmpi-1.10.2
+module load lammps-31Jan14
+module list
 # Executable
-mpirun lmp_linux < $1
+mpiexec lmp_linux < $1
 
 
 echo "Finished on:"
